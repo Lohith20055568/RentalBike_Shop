cf6cfa1350dced52a128bf454235501a2057c14b
20055568 <lohithbadam10@gmail.com>
2025-12-03 12:31:45 +0000

    async_commit

--- test/api.js
+++ test/api.js
@@ -7,3 +7,14 @@ const DATA_FILE = path.join(__dirname, '..', 'data.json');
 const BACKUP_FILE = DATA_FILE + '.bak';
 
 const app = require('../server');
+
+beforeEach(async () => {
+  const raw = await fs.readFile(DATA_FILE, 'utf8');
+  await fs.writeFile(BACKUP_FILE, raw, 'utf8');
+});
+
+afterEach(async () => {
+  const raw = await fs.readFile(BACKUP_FILE, 'utf8');
+  await fs.writeFile(DATA_FILE, raw, 'utf8');
+  await fs.unlink(BACKUP_FILE);
+});
